7da866bdbd69bc0fbb651e0ad4845ec7d3a05ea2
nikunj ladva crest <nikunj.l@crestinfosystems.com>
2023-05-20 17:57:16 +0530

    login router add

--- router/auth.router.js
+++ router/auth.router.js
@@ -1,6 +1,7 @@
 const authRouter = require('express').Router();
 const authController = require('../controller/auth.controller')
 
-authRouter.post("/registor", authController.registor)
+authRouter.post("/registor", authController.registor);
+authRouter.post("/login", authController.login);
 
 module.exports = authRouter
